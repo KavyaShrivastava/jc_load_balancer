06a85b6def75cd5e3192ae601367c7c576c7a667
Kavya Shrivastava <kshrivastava_2023@depauw.edu>
2024-02-12 17:25:20 -0600

    initial request and response added to server
    Co-authored-by: Vidit Khandelwal <csvidit@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-import http from "http";
-import e from "express";
+import http from "http"
+import e from "express"
+import cors from "cors"
 
 const app = e();
+
+app.use(cors());
 
 app.use(e.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const remoteAddress = req.connection.remoteAddress
+  const url = req.url
+  const host = req.headers.host
+  const user_agent = req.headers["user-agent"]
+  const response = `Received request from ${remoteAddress} GET ${url} Host: ${host} User-agent: ${user_agent} Accept: */*`
+  console.log(response)
+  res.status(200).send({
+    response
+  })
 });
 
 const server = http.createServer(app);
 
-server.listen(69420, () => {
-  console.log("Server is running on port 3000");
+server.listen(80, () => {
+  console.log("Server is running on port 80");
 });
